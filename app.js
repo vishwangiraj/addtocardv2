e5b0ba0c77697ecdc53306104341f9e8baa9cd26
vishwangiraj <149557882+vishwangiraj@users.noreply.github.com>
2023-11-08 08:32:00 +0530

    Update app.js

--- app.js
+++ app.js
@@ -18,37 +18,37 @@ let products = [
         id: 1,
         name: 'PRODUCT NAME 1',
         image: '1.PNG',
-        price: 120000
+        price: 120
     },
     {
         id: 2,
         name: 'PRODUCT NAME 2',
         image: '2.PNG',
-        price: 120000
+        price: 100
     },
     {
         id: 3,
         name: 'PRODUCT NAME 3',
         image: '3.PNG',
-        price: 220000
+        price: 220
     },
     {
         id: 4,
         name: 'PRODUCT NAME 4',
         image: '4.PNG',
-        price: 123000
+        price: 350
     },
     {
         id: 5,
         name: 'PRODUCT NAME 5',
         image: '5.PNG',
-        price: 320000
+        price: 320
     },
     {
         id: 6,
         name: 'PRODUCT NAME 6',
         image: '6.PNG',
-        price: 120000
+        price: 150
     }
 ];
 let listCards  = [];
